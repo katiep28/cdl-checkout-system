9bbb9ea6d742ca34c5e2a40dcc02e17b574e3961
Katie Postle <katiepostle@hotmail.co.uk>
2020-06-10 14:46:21 +0100

    Updates

---
+++ precache-manifest.6e3e2b428a5eaf7de3ca63218865eb0f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48c1516bf640e3cc8ed7a429947a67e",
+    "url": "/cdl-checkout-system/index.html"
+  },
+  {
+    "revision": "bf9baa7c548a001b59ad",
+    "url": "/cdl-checkout-system/static/css/main.4a8431af.chunk.css"
+  },
+  {
+    "revision": "f32dca8114c1d15a783f",
+    "url": "/cdl-checkout-system/static/js/2.dcb109a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cdl-checkout-system/static/js/2.dcb109a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9baa7c548a001b59ad",
+    "url": "/cdl-checkout-system/static/js/main.27497594.chunk.js"
+  },
+  {
+    "revision": "e5b4f9918b69861fe6e3",
+    "url": "/cdl-checkout-system/static/js/runtime-main.edbd4cba.js"
+  },
+  {
+    "revision": "d4c3ffc0dee9924d766e0c675f6b639b",
+    "url": "/cdl-checkout-system/static/media/apples.d4c3ffc0.png"
+  },
+  {
+    "revision": "eecc7b0ddf56668105cada9616151f2a",
+    "url": "/cdl-checkout-system/static/media/bananas.eecc7b0d.png"
+  },
+  {
+    "revision": "31d71ada71890b198a8bf3e81a3cbcae",
+    "url": "/cdl-checkout-system/static/media/carrots.31d71ada.png"
+  },
+  {
+    "revision": "06b4910e6ee9b2995aa97ce1fd03800d",
+    "url": "/cdl-checkout-system/static/media/dates.06b4910e.png"
+  }
+]);
